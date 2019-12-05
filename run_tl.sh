5010ba331c0500209575ec4d4c02dd11a3e19ef2
littlepai <txgyswh@163.com>
2019-12-05 20:32:28 +0800

    迁移的时候多迭代几个epoch吧

--- run_tl.sh
+++ run_tl.sh
@@ -10,4 +10,4 @@ echo "解压一毛一样的迁移样本当验证集（数据少）,存放于 $tl
 unzip data/迁移学习样本.zip -d $tl_val_sample_dir > /dev/null
 
 echo "开始迁移学习"
-python main.py  --restore True --lr 1e-5 --traindir $tl_train_sample_dir --valdir $tl_val_sample_dir --epoch 10 --batchSize 32
+python main.py  --restore True --lr 1e-5 --traindir $tl_train_sample_dir --valdir $tl_val_sample_dir --batchSize 32
