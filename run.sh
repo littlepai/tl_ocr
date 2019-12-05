e84bcb8210582f41709b974740ca2a78f028c920
littlepai <txgyswh@163.com>
2019-12-05 15:15:00 +0800

    训练脚本纠错

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ echo "开始生成训练数据 $train_sample_num 条，存于 $train_sample_di
 python data_gen.py $train_sample_num  $train_sample_dir
 
 echo "开始生成验证数据 $val_sample_num 条，存于 $val_sample_dir"
-python data_gen.py $val_sample_num $val_sample_num
+python data_gen.py $val_sample_num $val_sample_dir
 
 python main.py --epoch 10
 
--- run_tl.sh
+++ run_tl.sh
@@ -5,8 +5,8 @@ rm -rf $tl_train_sample_dir
 echo "解压迁移样本当训练集,存放于 $tl_train_sample_dir"
 unzip data/迁移学习样本.zip -d $tl_train_sample_dir > /dev/null
 
-echo "解压一毛一样的迁移样本当验证集（数据少）,存放于 $tl_val_sample_dir"
 rm -rf $tl_val_sample_dir
+echo "解压一毛一样的迁移样本当验证集（数据少）,存放于 $tl_val_sample_dir"
 unzip data/迁移学习样本.zip -d $tl_val_sample_dir > /dev/null
 
 echo "开始迁移学习"
